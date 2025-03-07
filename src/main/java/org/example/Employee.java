9f5a7e8cdd599a061abab63eeab30978e96ec3d3
mustafa-bircan <msbrtfcn@gmail.com>
2025-03-07 13:59:59 +0300

    Update Employee.java

--- src/main/java/org/example/Employee.java
+++ src/main/java/org/example/Employee.java
@@ -1,4 +1,80 @@
 package org.example;
 
 public class Employee {
+    private int id;
+    private String fullName;
+    private String email;
+    private String password;
+    private String[] healthplans;
+
+
+    public Employee(int id, String fullName, String email, String password, String[] healthplans) {
+        this.id = id;
+        this.fullName = fullName;
+        this.email = email;
+        this.password = password;
+        this.healthplans = healthplans;
+    }
+
+    public void addHealthPlan(int index, String name) {
+        if (index < 0 || index >= healthplans.length) {
+            System.out.println("Geçersiz Seçim");
+            return;
+        }
+        if (healthplans[index] != null) {
+            System.out.println("Burada zaten bir değer var");
+        } else {
+            healthplans[index] = name;
+            System.out.println("Sağlık Planı " + name + " eklendi.");
+        }
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String[] getHealthPlans() {
+        return healthplans;
+    }
+
+    public void setHealthplans(String[] healthplans) {
+        this.healthplans = healthplans;
+    }
+
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", fullName='" + fullName + '\'' +
+                ", email='" + email + '\'' +
+                ", healthplans=" + String.join(", ", healthplans) +
+                '}';
+    }
 }
