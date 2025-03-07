399cea0516a5214ab33b5185026cd80e1e2aef39
mustafa-bircan <msbrtfcn@gmail.com>
2025-03-07 13:58:44 +0300

    Update Pllan.java

--- src/main/java/org/example/enums/Plan.java
+++ src/main/java/org/example/enums/Plan.java
@@ -1,4 +1,23 @@
 package org.example.enums;
-
+//Sağlık planlarının adlarını ve fiyatlarını tutacak..
 public enum Plan {
+    BASIC("BASIC", 100.0),
+    ADVANCED ("ADVANCED", 200.0),
+    PRO ("PRO", 200.0);
+
+    private String name;
+    private double price;
+
+    Plan(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
 }
